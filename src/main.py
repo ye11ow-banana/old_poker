c660058a521161a7611eb07b1f4c3c65940bf126
Mykhailo <mikhail.yakubets@gmail.com>
2023-08-05 21:00:41 +0300

    Add auth router

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
-from src.analysis.router import router as router_analysis
+from analysis.router import router as router_analysis
+from auth.router import router as router_auth
 
 app = FastAPI(
     title="Poker app",
 )
 
 app.include_router(router_analysis)
+app.include_router(router_auth)
