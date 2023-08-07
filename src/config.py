e22bdb580410b309efb195f5bfa1eee0c7ed63c9
Mykhailo <mikhail.yakubets@gmail.com>
2023-08-07 16:44:34 +0300

    Setup `authentication` dependencies, schemas, exceptions, consts

--- src/config.py
+++ src/config.py
@@ -5,3 +5,8 @@ DB_PORT = config("DB_PORT")
 DB_NAME = config("DB_NAME")
 DB_USER = config("DB_USER")
 DB_PASS = config("DB_PASS")
+
+SECRET_KEY = config("SECRET_KEY")
+ALGORITHM = config("ALGORITHM")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
