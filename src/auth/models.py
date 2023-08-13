8e0313530021e338660295d44b9174793a395384
Mykhailo <mikhail.yakubets@gmail.com>
2023-08-13 15:27:05 +0300

    Fix id and username fields in `User` model

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from sqlalchemy import UUID, Column, String
 
 from database import Base
@@ -6,6 +8,8 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID, primary_key=True, index=True)
-    username = Column(String, nullable=False)
+    id = Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid4, index=True
+    )
+    username = Column(String, unique=True, nullable=False)
     hashed_password = Column(String(length=1024), nullable=False)
