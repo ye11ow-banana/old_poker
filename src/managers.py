3fb3b03e11be5c4bcae108e204a324a3b6c59b83
Mykhailo <mikhail.yakubets@gmail.com>
2024-03-25 19:35:42 +0200

    Add `WSConnectionManager`

---
+++ src/managers.py
@@ -0,0 +1,25 @@
+from fastapi.websockets import WebSocket
+
+from schemas import Response
+
+
+class WSConnectionManager:
+    def __init__(self) -> None:
+        self._active_connections: dict[str, WebSocket] = {}
+
+    async def connect(self, websocket: WebSocket, user_id: str) -> None:
+        await websocket.accept()
+        self._active_connections[user_id] = websocket
+
+    def disconnect(self, user_id: str) -> None:
+        del self._active_connections[user_id]
+
+    async def broadcast(self, data: Response) -> None:
+        for connection in self._active_connections.values():
+            await connection.send_json(data.model_dump(by_alias=True))
+
+    def get_connections_count(self) -> int:
+        return len(self._active_connections)
+
+
+ws_manager = WSConnectionManager()
