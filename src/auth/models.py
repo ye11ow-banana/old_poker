497e22e064ff1a81897f923cc540b5bbe79e4717
Mykhailo <mikhail.yakubets@gmail.com>
2024-04-01 22:33:02 +0300

    Add `friendships` M2M table

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,8 +1,16 @@
 from uuid import uuid4
 
-from sqlalchemy import UUID, Column, String, Integer
+from sqlalchemy import UUID, Column, String, Integer, Table, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database import Base
+
+friends_association_table = Table(
+    "friendships",
+    Base.metadata,
+    Column("left_user_id", UUID(as_uuid=True), ForeignKey("users.id")),
+    Column("right_user_id", UUID(as_uuid=True), ForeignKey("users.id")),
+)
 
 
 class User(Base):
@@ -14,3 +22,10 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     hashed_password = Column(String(length=1024), nullable=False)
     elo = Column(Integer, default=1000, nullable=False)
+    friends = relationship(
+        "User",
+        secondary=friends_association_table,
+        primaryjoin="id==friends_association_table.c.left_user_id",
+        secondaryjoin="id==friends_association_table.c.right_user_id",
+        backref="friends",
+    )
